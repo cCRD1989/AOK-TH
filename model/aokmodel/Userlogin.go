c89741b053aab4317daf636e0196f3b5a890eed0
cCRD <yokoyokororog@hotmail.com>
2023-03-29 19:28:39 +0700

    be

--- model/aokmodel/Userlogin.go
+++ model/aokmodel/Userlogin.go
@@ -1,19 +1,21 @@
 package aokmodel
 
+import "time"
+
 type Userlogin struct {
-	//ID              string
-	Username string
-	// Password        string
-	// Gold            int
-	Cash int
-	// Email           string
-	// IsEmailVerified int
-	// AuthType        int
-	// AccessToken     string
-	// UserLevel       int
-	// UnbanTime       int
-	// CreateAt        time.Time
-	// UpdateAt        time.Time
+	ID              string
+	Username        string
+	Password        string
+	Gold            int
+	Cash            int
+	Email           string
+	IsEmailVerified int
+	AuthType        int
+	AccessToken     string
+	UserLevel       int
+	UnbanTime       int
+	CreateAt        time.Time
+	UpdateAt        time.Time
 }
 
 func (n *Userlogin) TableName() string {
