4ff9b5021b4039b68e290f2a88f36ad2a72752c1
cCRD <yokoyokororog@hotmail.com>
2023-03-31 17:48:10 +0700

    eg

--- model/logregister.go
+++ model/logregister.go
@@ -8,10 +8,10 @@ type LogRegister struct {
 	gorm.Model
 
 	Sub      string `gorm:"uniqueIndex;type:varchar(100);not null"`
-	Email    string `gorm:"type:varchar(100)"`
-	Name     string `gorm:"type:varchar(100)"`
-	Img      string `gorm:"type:varchar(100)"`
-	Username string `gorm:"type:varchar(100)"`
-	Password string `gorm:"type:varchar(100)"`
-	Status   string `gorm:"type:varchar(50)"`
+	Email    string `gorm:"type:varchar(100);default:''"`
+	Name     string `gorm:"type:varchar(100);default:''"`
+	Img      string `gorm:"type:varchar(100);default:''"`
+	Username string `gorm:"type:varchar(100);default:''"`
+	Password string `gorm:"type:varchar(100);default:''"`
+	Status   string `gorm:"type:varchar(50);default:''"`
 }
