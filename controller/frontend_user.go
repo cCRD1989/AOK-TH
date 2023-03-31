7893d8bc3e14efe3510c3c21784414894f7c0a1b
cCRD <yokoyokororog@hotmail.com>
2023-03-31 16:37:16 +0700

    rg

--- controller/frontend_user.go
+++ controller/frontend_user.go
@@ -142,7 +142,7 @@ func (f *Frontend) Auth_google_Regis(ctx *gin.Context) {
 	}
 
 	//บันทึก Log  LogRegister Success
-	db.Conn.Model(&model.LogRegister{}).Where("sub = ?", idcode).Update("status", "Success")
+	db.Conn.Model(&model.LogRegister{}).Where("sub = ?", idcode).Update("status", "Success").Update("username ", username1).Update("password", passSig)
 
 	ctx.HTML(http.StatusOK, "frontend/auth.html", gin.H{
 		"title":  "Age Of Khagan Thailand | Sign up successfully",
--- model/logregister.go
+++ model/logregister.go
@@ -7,11 +7,11 @@ import (
 type LogRegister struct {
 	gorm.Model
 
-	Sub      string
-	Email    string
-	Name     string
-	Img      string
-	Username string
-	Password string
-	Status   string
+	Sub      string `gorm:"uniqueIndex;type:varchar(100);not null"`
+	Email    string `gorm:"type:varchar(100)"`
+	Name     string `gorm:"type:varchar(100)"`
+	Img      string `gorm:"type:varchar(100)"`
+	Username string `gorm:"type:varchar(100)"`
+	Password string `gorm:"type:varchar(100)"`
+	Status   string `gorm:"type:varchar(50)"`
 }
