f6523dbfd22fd9efc1049dd5f33239e83fb0b28b
cCRD <yokoyokororog@hotmail.com>
2023-04-18 15:56:59 +0700

    fb

--- controller/frontend_user.go
+++ controller/frontend_user.go
@@ -44,7 +44,7 @@ var (
 	OauthConf = &oauth2.Config{
 		ClientID:     "",
 		ClientSecret: "",
-		RedirectURL:  "https://ageofkhagan/auth/facebookCall",
+		RedirectURL:  "https://www.ageofkhagan/auth/facebookCall",
 		//RedirectURL: "https://localhost/",
 		Scopes:   []string{"public_profile", "email"},
 		Endpoint: facebook.Endpoint,
